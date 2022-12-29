f6d6bab4df12f865c338c145fba57da874d0f277
Dove. J. Kim <kimhippowork@gmail.com>
2022-12-29 10:49:18 +0900

    ['22.12.29] 디자인 파일 수정 완료

--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from datetime import datetime
 # Create your views here.s
 
 def index(request):
-    return render(request, 'profile.html', context = {})
+    return render(request, 'streams.html', context = {})
