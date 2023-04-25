c26dce0140ed8884e1735a9f9fb70f720b893056
Dove. J. Kim <kimhippowork@gmail.com>
2023-04-25 16:46:31 +0900

    ['23.04.25][FIX] json 파일 경로 등 CONFIG 내용 추가

--- utils/configs.py
+++ utils/configs.py
@@ -1,6 +1,8 @@
 ROOT_PATH        = '/config/workspace/project'
 DATA_PATH        = f'{ROOT_PATH}/DoveNest/informations/jsons'
-DB_PATH          = f'{ROOT_PATH}/DoveNest/informations/db'
-JSON_PATH        = f'{ROOT_PATH}/utils/keys'
+JSON_PATH        = f'{ROOT_PATH}/utils'
 JSON_BACKUP_PATH = f'{ROOT_PATH}/BACKUPS/keys'
+DB_PATH          = f'{ROOT_PATH}/DoveNest/informations/db'
+CONFIG_NAMES     = ['youtube', 'steam', 'openai', 'chimhaha_int_port', 'chimwiki_int_port', 'dovenest_int_port', 'dovenest_api_int_port']
+
 
