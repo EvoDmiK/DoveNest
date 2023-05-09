3db0514f519e3b5236b4b2177d0bfdca4b1ae9f5
Dove. J. Kim <kimhippowork@gmail.com>
2023-05-09 17:48:29 +0900

    ['23.05.09][FEAT] 할인정보 DB 저장 기능 로깅, 컨피그 코드

---
+++ informations/src/py/misc/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+def get_logger():
+
+    logger = logging.getLogger('DoveNest')
+    logger.setLevel(logging.INFO)
+
+    formatter      = logging.Formatter('[%(asctime)s][%(levelname)s] %(name)s : %(message)s')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    return logger 
