03c8632df2fe90fa56f4e6f47f664c66696cf049
Dove. J. Kim <kimhippowork@gmail.com>
2023-05-11 17:25:01 +0900

    ['23.05.11][REFAC] config, port 파일 로딩 함수 리팩토링

--- misc/configs.py
+++ misc/configs.py
@@ -23,11 +23,13 @@ save_json     = lambda data, json_path: json.dump(data, open(json_path, 'w'), in
 
 ## key를 담고 있는 json 파일이 깨지거나 한 경우에 복구시켜 주는 함수
 def repair_json(json_path: str, ftype: str = 'config') -> dict:
-    ## == 입력 값 ==
-    ## json_path : config 파일이 저장되어 있는 경로
+    '''
+       == 입력 값 ==
+       json_path : config 파일이 저장되어 있는 경로
 
-    ## == 출력 값 ==
-    ## config 파일에 저장되어 있는 세팅 값들.
+       == 출력 값 ==
+       config 파일에 저장되어 있는 세팅 값들.
+    '''
     
     try: json_ = load_json(f'{json_path}/{ftype}.json')
     
@@ -47,26 +49,6 @@ def repair_json(json_path: str, ftype: str = 'config') -> dict:
             os.makedirs(save_path, exist_ok = True)
             save_json(json_, f'{save_path}/{ftype}.json')
         
-    finally: return json_
-
-
-## steam API 키 가져오는 함수
-def repair_json(json_path, ftype = 'config'):
-
-    try: json_ = load_json(f'{json_path}/{ftype}.json')
-    except Exception as e:
-
-        LOGGER.error(f'[ERR.K.A-0001] json 파일이 깨져 열 수 없습니다. {e} \n{format_exc()}')
-        texts = open(f'{JSON_BACKUP_PATH}/{ftype}.txt', 'r').readlines()
-        texts = [text.split() for text in texts]
-        json_ = {name : text for name, text in texts}
-
-        save_paths = [CONFIG_PATH, CONFIG_BACKUP_PATH]
-        for save_path in save_paths:
-
-            os.makedirs(save_path, exist_ok = True)
-            save_json(json_, f'{save_path}/{ftype}.json')
-
     finally: return json_
 
 
