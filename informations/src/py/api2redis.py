99cbb33a1f6867139f8218ac86a4f91fc8e6955f
Dove. J. Kim <kimhippowork@gmail.com>
2023-05-11 17:36:22 +0900

    ['23.05.11][FEAT] 하루에 한 번씩 스팀 게임 목록을 업데이트 하도록 수정

--- informations/src/py/api2redis.py
+++ informations/src/py/api2redis.py
@@ -21,6 +21,7 @@ def get_logger():
     logger.addHandler(stream_handler)
     return logger
 
+
 LOGGER = get_logger()
 ROOT_PATH   = '/config/workspace/project'
 CONFIG_PATH = f'{ROOT_PATH}/utils/configs/config.json'
@@ -29,6 +30,10 @@ PORTS_PATH  = f'{ROOT_PATH}/utils/configs/ports.json'
 CONFIG      = edict(json.loads(open(CONFIG_PATH, 'r').read()))
 PORTS       = edict(json.loads(open(PORTS_PATH, 'r').read()))
 URLS        = {'appdetail' : 'https://store.steampowered.com/api/appdetails?appids' }
+
+HOST        = CONFIG.global_host
+PORT        = PORTS.redis_port
+conn        = redis.Redis(host = HOST, port = PORT, db = 1)
 
 def return_or_print(url):
     
@@ -42,17 +47,6 @@ def get_info(appid):
     res = return_or_print(f"https://store.steampowered.com/api/appdetails?appids={appid}&l=korean")
     return res
 
-key         = CONFIG.steam
-host        = CONFIG.global_host
-port        = PORTS.redis_port
-conn        = redis.StrictRedis(host = host, port = port, db = 1)
-
-apps = return_or_print('https://api.steampowered.com/ISteamApps/GetAppList/v2')['applist']['apps']
-
-print(f'original length : {len(apps)}')
-keys = [key.decode().split(':')[1] for key in conn.keys()]
-apps = [app['appid'] for app in apps if str(app['appid']) not in keys]
-print(f'작업해야하는 length : {len(apps)}')
 
 def save_redis(idx, apps):
  
@@ -66,27 +60,47 @@ def save_redis(idx, apps):
                     }
             
             conn.set(f'id:{app}', json.dumps(data))
-        except Exception as e: LOGGER.error(e)
+            
+        except Exception as e: 
+            if type(e) == KeyError: continue
+            LOGGER.error(e)
         
         finally: 
-            time.sleep(5)
+            time.sleep(7)
             
         print(f'[Done] process : {idx}th job is finished.\n')
-            
-job1 = apps[: len(apps) // 4]
-job2 = apps[len(apps) // 4: len(apps) // 2]
-job3 = apps[len(apps) // 2: 3 * len(apps) // 4]
-job4 = apps[3 * len(apps) // 4 : ]
 
-jobs  = [job1, job2, job3, job4]
-stime = time.time()
-procs = []
-for idx, job in enumerate(jobs, 1):
-    
-    p = Process(target = save_redis, args = (idx, job))
-    p.start()
-    procs.append(p)
-    
-for p in procs: p.join()
 
-time.time() - stime
+def begin_jobs(apps):        
+    job1 = apps[: len(apps) // 4]
+    job2 = apps[len(apps) // 4: len(apps) // 2]
+    job3 = apps[len(apps) // 2: 3 * len(apps) // 4]
+    job4 = apps[3 * len(apps) // 4 : ]
+
+    jobs  = [job1, job2, job3, job4]
+    stime = time.time()
+    procs = []
+    for idx, job in enumerate(jobs, 1):
+        
+        p = Process(target = save_redis, args = (idx, job))
+        p.start()
+        procs.append(p)
+        
+    for p in procs: p.join()
+
+    time.time() - stime
+
+
+apps = return_or_print('https://api.steampowered.com/ISteamApps/GetAppList/v2')['applist']['apps']
+
+print(f'original length : {len(apps)}')
+keys = set([key.decode().split(':')[1] for key in conn.keys()])
+apps = [app['appid'] for app in apps if str(app['appid']) not in keys]
+print(f'작업해야하는 length : {len(apps)}')
+
+# begin_jobs(apps)
+
+schedule.every().day.at("01:00").do(begin_jobs, apps)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
